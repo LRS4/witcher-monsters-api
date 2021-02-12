e4d5b6dc194069bda08878d8c1eb893745148de5
LRS4 <lrspencer@hotmail.co.uk>
2021-02-12 15:02:18 +0000

    Add monster model and initial endpoint

--- src/main/java/com/example/monsters/MonstersApplication.java
+++ src/main/java/com/example/monsters/MonstersApplication.java
@@ -1,13 +1,35 @@
 package com.example.monsters;
 
+import com.example.monsters.monster.Monster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class MonstersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonstersApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Monster> hello() {
+		return List.of(
+				new Monster(
+						"Bears",
+						"A bear",
+						"URL",
+						"Skellige",
+						"None",
+						"None",
+						LocalDate.now(),
+						1
+				)
+		);
+	}
 }
---
+++ src/main/java/com/example/monsters/monster/Monster.java
@@ -0,0 +1,143 @@
+package com.example.monsters.monster;
+
+import java.time.LocalDate;
+
+public class Monster {
+    private Long id;
+    private String name;
+    private String entry;
+    private String imageUrl;
+    private String occurrence;
+    private String susceptibility;
+    private String loot;
+    private LocalDate dateAdded;
+    private Integer classCategory;
+
+    public Monster() {
+    }
+
+    public Monster(Long id,
+                   String name,
+                   String entry,
+                   String imageUrl,
+                   String occurrence,
+                   String susceptibility,
+                   String loot,
+                   LocalDate dateAdded,
+                   Integer classCategory) {
+        this.id = id;
+        this.name = name;
+        this.entry = entry;
+        this.imageUrl = imageUrl;
+        this.occurrence = occurrence;
+        this.susceptibility = susceptibility;
+        this.loot = loot;
+        this.dateAdded = dateAdded;
+        this.classCategory = classCategory;
+    }
+
+    public Monster(String name,
+                   String entry,
+                   String imageUrl,
+                   String occurrence,
+                   String susceptibility,
+                   String loot,
+                   LocalDate dateAdded,
+                   Integer classCategory) {
+        this.name = name;
+        this.entry = entry;
+        this.imageUrl = imageUrl;
+        this.occurrence = occurrence;
+        this.susceptibility = susceptibility;
+        this.loot = loot;
+        this.dateAdded = dateAdded;
+        this.classCategory = classCategory;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEntry() {
+        return entry;
+    }
+
+    public void setEntry(String entry) {
+        this.entry = entry;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getOccurrence() {
+        return occurrence;
+    }
+
+    public void setOccurrence(String occurrence) {
+        this.occurrence = occurrence;
+    }
+
+    public String getSusceptibility() {
+        return susceptibility;
+    }
+
+    public void setSusceptibility(String susceptibility) {
+        this.susceptibility = susceptibility;
+    }
+
+    public String getLoot() {
+        return loot;
+    }
+
+    public void setLoot(String loot) {
+        this.loot = loot;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDate dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public Integer getClassCategory() {
+        return classCategory;
+    }
+
+    public void setClassCategory(Integer classCategory) {
+        this.classCategory = classCategory;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", entry='" + entry + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", occurrence='" + occurrence + '\'' +
+                ", susceptibility='" + susceptibility + '\'' +
+                ", loot='" + loot + '\'' +
+                ", dateAdded=" + dateAdded +
+                ", classCategory=" + classCategory +
+                '}';
+    }
+}
