bd8d316d47c541a0b8db7a0d2bc8f65b5cbea7c6
LRS4 <lrspencer@hotmail.co.uk>
2021-02-16 18:02:21 +0000

    Complete beasts population script

--- src/main/java/com/example/monsters/config/BeastConfig.java
+++ src/main/java/com/example/monsters/config/BeastConfig.java
@@ -55,9 +55,115 @@ public class BeastConfig {
                     beasts
             );
 
+            Monster wildBoars = new Monster(
+                    "wild-boars",
+                    "Wild Boars",
+                    "How doth the little wild boar\n" +
+                            "Improve his shining tusks,\n" +
+                            "And rut them 'gainst the forest floor,\n" +
+                            "And on the trees' rough husks!\n" +
+                            "How cheerfully he seems to grin\n" +
+                            "How neatly spreads his claws,\n" +
+                            "And welcomes little lasses in,\n" +
+                            "With gently smiling jaws!\n" +
+                            "Louis of Charolle, mathematician, author and poet\n" +
+                            "These woodland beasts are as \"wild\" – untamed, nasty and aggressive – as their name indicates. To this day, many peasants in outlying Kaedweni villages believe wild boars persist on a diet of young maids, though in truth they most often slate their hunger with roots and acorns. Do not think this vegetarian diet makes them harmless, however. In fact, these animals' stout build and sharp teeth render them veritable fur-covered combat machines. Mother Nature has additionally equipped them with dual pairs of tusks - upper \"pipes\" and lower \"sabres\" - and this weaponry's sum effect is to make the beasts the terrors of the woods, a threat to all who venture or dwell there, human, humanoid and otherwise. Boars also have hard, club-like snouts, which some peasants call their \"whistles,\" though no boar has ever been heard to make such a noise. Instead, they emit a characteristic grunt, which sounds to some like the snoring of an extremely overweight man. Another peasant belief claims boars have extremely prickly natures, and, if offended, will vent their anger by knocking down fences and gobbling up potatoes. Though they sometimes live alone, boars usually appear in small groups of 3 to 5 specimens.",
+                    "https://static.wikia.nocookie.net/witcher/images/d/da/Tw3_journal_panther.png/revision/latest/scale-to-width-down/350?cb=20160531173331",
+                    "Gustfields,Toussaint",
+                    "Northern Wind,Beast oil,Igni,Yrden",
+                    "Boar tusk,Boar pelt,Raw meat",
+                    LocalDate.now(),
+                    beasts
+            );
+
+            Monster bigBadWolf = new Monster(
+                    "big-bad-wolf",
+                    "Big Bad Wolf",
+                    "You ask why I have these big hands, my friend? Well, I'll tell you my secret: exercise.\n" +
+                            "— Big Bad Wolf\n" +
+                            "The Big Bad Wolf who lived in the Land of a Thousand Fables was, like the other denizens of that strange sphere, created by Artorius Vigo based on a figure from folk tales. Once he served as a playmate to the duke's daughters, acting out scenes with a certain red-hooded girl and her grandmother, but as the fable land slowly degenerated, so did he.\n" +
+                            "Geralt and Syanna met the Big Bad Wolf while he was nursing a nasty hangover and was not eager to cooperate. Yet since our heroes needed a bean which the wolf had, they forced him to act out his tale.\n" +
+                            "That tale ended as it always had: the Big Bad Wolf attacked the Hunter (played by Geralt), biting and clawing him fiercely while his own wounds healed almost instantaneously. Inevitably, however, the Big Bad Wolf was killed and his stomach cut open.\n" +
+                            "This time, out came not a grandma and her granddaughter, but a magic bean.",
+                    "https://static.wikia.nocookie.net/witcher/images/d/db/Tw3_journal_bigbadwolf.png/revision/latest/scale-to-width-down/350?cb=20160605085157",
+                    "Land of a Thousand Fables",
+                    "Devil's Puffball,Beast oil",
+                    "Magic dust,Red mutagen,Fake tooth,Corkscrew,Bottle caps",
+                    LocalDate.now(),
+                    beasts
+            );
+
+            Monster beastOfBeauclair = new Monster(
+                    "the-beast-of-beauclair",
+                    "The Beast of Beauclair",
+                    "Eyewitnesses to gruesome monster attacks always have a hard time describing the creature in question. The beasts move quickly and often attack at night, while the witnesses are terrified and primarily concerned with fleeing for their lives. As a result, witchers quite often have no inkling what creature they face until they find tracks or otherwise establish something for themselves. Such was the case with the Beast tormenting Beauclair. Geralt knew it was deadly, elusive and fiendishly clever. Everything else he heard was clearly the product of imaginations fed by fear of a dangerous predator.\n" +
+                            "When Geralt identified the Beast, he discovered it was a higher vampire, meaning one of the most difficult foes a witcher can come to face.\n" +
+                            "Higher vampires are superhumanly strong and fast. They can turn completely invisible, and each comes with its own personalized set of talents and abilities perfected through centuries of existence.\n" +
+                            "When Geralt realized he was dealing with such a creature, he swore foully and realized the rumors about the Beast had not been exaggerated in the slightest.",
+                    "https://static.wikia.nocookie.net/witcher/images/b/b5/Tw3_journal_dettlaff_vampire.png/revision/latest/scale-to-width-down/350?cb=20160601042441",
+                    "Toussaint",
+                    "Vampire oil,Black Blood,Quen",
+                    "None",
+                    LocalDate.now(),
+                    beasts
+            );
+
+            Monster wolves = new Monster(
+                    "wolves",
+                    "Wolves",
+                    "Friend of mine used to say that with all these griffins and basilisks and whatnot, good old fashioned wolves weren't nothing to worry about... Then the damned beasts devoured half his flock.\n" +
+                            "– Yngvar, shepherd\n" +
+                            "Once upon a time wolves were the absolute rulers of the forest. Men used them to frighten children, while adults, too, trembled at the sound of their howling. Post-Conjunction monsters not only pushed wolves into the deepest wilds but also took over their place in human nightmares. Yet this does not mean the old predators ceased to be a danger. Wolves do not have a drop of magic within them, breathe no fire and spit no acid, but that in no way stops them from killing unwary travelers and hunters.\n" +
+                            "Particularly dangerous are wargs, a nasty and ferocious subspecies of wolf, and the snow-white wolves which today can only be found in the wild highlands of the Skellige archipelago.\n" +
+                            "Wolves usually feed in packs counting from a handful to over a dozen members, though some particularly strong males hunt alone. Though wolves are weaker than many post-Conjunction monsters, they make up for any lack of strength with their intelligence. When fighting them one must be especially careful not to become surrounded. The presence of wolves can also signal more serious trouble – these predators often share their hunting grounds with werewolves and leshens.",
+                    "https://static.wikia.nocookie.net/witcher/images/6/62/Tw3_journal_wolf.png/revision/latest/scale-to-width-down/350?cb=20160409074701",
+                    "Velen,White Orchard,Skellige,Toussaint",
+                    "Beast oil",
+                    "Raw meat,Wolf hide,Wolf's liver,Dog tallow",
+                    LocalDate.now(),
+                    beasts
+            );
+
+            Monster dogs = new Monster(
+                    "dogs",
+                    "Dogs",
+                    "They say a dog's a man's best friend... But in these foul times one best be wary, even of his friends.\n" +
+                            "– Alfred Pankratz, beggar from Novigrad\n" +
+                            "Some claim dogs are reflections of their masters. A well-treated pet repays his owner with loyalty and trust, whereas an ill-treated one repays this treatment in kind as well, meeting yells with barks and kicks with bites. Since there is an abundance of bad men, so, too, is there an abundance of bad dogs. Most dangerous of all are the vagrant mutts who have lost all respect for men and developed a taste for blood and carrion.\n" +
+                            "Dogs usually attack in packs, choosing isolated victims, especially ones in whom they sense some kind of weakness. Packs of rabid dogs usually only dare attack children, drunkards, the elderly and cripples. Yet when backed into a corner, they will defend themselves with frothing bites and sharp claws against all and sundry.",
+                    "https://static.wikia.nocookie.net/witcher/images/e/e4/Tw3_journal_dog.png/revision/latest/scale-to-width-down/350?cb=20160304193515",
+                    "Velen,Novigrad,Skellige",
+                    "Beast oil",
+                    "Dog tallow,Raw meat",
+                    LocalDate.now(),
+                    beasts
+            );
+
+            Monster threeLittlePigs = new Monster(
+                    "three-little-pigs",
+                    "Three Little Pigs",
+                    "Why bother with brick? In this climate, even a straw house's nice and cozy.\n" +
+                            "– a little pig\n" +
+                            "\n" +
+                            "The three little pigs are perfect examples of a rather unfortunate phenomenon. Leave even a kind-hearted, warm, naively benign creature to fend for itself, and it won't remain so benign for long.\n" +
+                            "The three little pigs from the Land of a Thousand Fables were left for years without Artorius Vigo's supervision. During this time they went completely feral, transforming into three wild boars which attacked anyone who dared approach their ruined home.",
+                    "https://static.wikia.nocookie.net/witcher/images/b/b3/Tw3_journal_three_little_pigs.png/revision/latest/scale-to-width-down/350?cb=20161031083059",
+                    "Land of a Thousand Fables",
+                    "Beast oil",
+                    "Magic dust",
+                    LocalDate.now(),
+                    beasts
+            );
+
             repository.saveAll(
                     List.of(bear,
-                            panther)
+                            panther,
+                            wildBoars,
+                            bigBadWolf,
+                            beastOfBeauclair,
+                            wolves,
+                            dogs,
+                            threeLittlePigs)
             );
         };
     }
