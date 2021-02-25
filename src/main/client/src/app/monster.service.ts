f83ea11afd558d3d1041b1337d1171b0d5a02a9c
LRS4 <lrspencer@hotmail.co.uk>
2021-02-25 16:18:37 +0000

    Add client monster model and service

---
+++ src/main/client/src/app/monster.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Monster } from './monster';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MonsterService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getMonsters(): Observable<Monster[]> {
+    return this.http.get<Monster[]>(`${ this.apiServerUrl }/api/v1/monster`);
+  }
+
+  public addMonster(monster: Monster, categoryName: string): Observable<any> {
+    return this.http.post<any>(
+      `${ this.apiServerUrl }/api/v1/monster/category/${ categoryName }`,
+      monster
+    );
+  }
+
+  public updateMonster(monster: Monster, monsterId: number, categoryId: number): Observable<any> {
+    return this.http.put<any>(
+      `${ this.apiServerUrl }/api/v1/monster/${ monsterId }/${ categoryId }`,
+      monster
+    );
+  }
+
+  public deleteMonster(monsterId: number): Observable<any> {
+    return this.http.delete<any>(`${ this.apiServerUrl }/api/v1/monster/${ monsterId }`);
+  }
+}
---
+++ src/main/client/src/app/monster.ts
@@ -0,0 +1,11 @@
+export interface Monster {
+    id: number;
+    name: string;
+    displayName: string;
+    entry: string;
+    imageUrl: string;
+    occurence: string;
+    susceptibility: string;
+    loot: string;
+    category: Object;
+}
