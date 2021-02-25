441e94354a45c229fa2592d11c85f6be4a8d3a54
LRS4 <lrspencer@hotmail.co.uk>
2021-02-25 16:28:39 +0000

    Add component get monsters view model

--- src/main/client/src/app/app.component.ts
+++ src/main/client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Monster } from './monster';
+import { MonsterService } from './monster.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'Monsters';
+export class AppComponent implements OnInit {
+  public title = 'Monsters';
+  public monsters: Monster[] = [];
+
+  constructor(private monsterService: MonsterService) { }
+
+  ngOnInit() {
+    this.getMonsters();
+  }
+
+  public getMonsters(): void {
+    this.monsterService.getMonsters().subscribe(
+      (response: Monster[]) => {
+        this.monsters = response;
+      }
+    ),
+    (error: HttpErrorResponse) => {
+      console.log(error.message);
+    }
+  }
 }
