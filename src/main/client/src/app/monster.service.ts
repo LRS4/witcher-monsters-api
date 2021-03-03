6de8499cfb7775bbb9613d9519946d357d2c94ee
LRS4 <lrspencer@hotmail.co.uk>
2021-03-03 19:32:06 +0000

    Add API base url to environment

--- src/main/client/src/app/monster.service.ts
+++ src/main/client/src/app/monster.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Monster } from './monster';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MonsterService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
