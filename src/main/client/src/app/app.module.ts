969f65221c8ac7edffb138aa1d9fda534b90bd08
LRS4 <lrspencer@hotmail.co.uk>
2021-03-04 15:56:11 +0000

    Add navbar

--- src/main/client/src/app/app.module.ts
+++ src/main/client/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonsterService } from './monster.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
