0c5704a3c28c1132bf675bc4121960e72c27016f
LRS4 <lrspencer@hotmail.co.uk>
2021-03-06 18:14:05 +0000

    Add vulnerabilities constants

---
+++ src/main/client/src/app/constants/vulnerabilities.ts
@@ -0,0 +1,74 @@
+export default {
+    'QUEN': {
+        'type': 'SIGN',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615045875/App%20Images/Witcher%203%20Bestiary/Weaknesses/quen.png',
+    },
+    'IGNI': {
+        'type': 'SIGN',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615045641/App%20Images/Witcher%203%20Bestiary/Weaknesses/igni.png',
+    },
+    'YRDEN': {
+        'type': 'SIGN',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615045817/App%20Images/Witcher%203%20Bestiary/Weaknesses/yrden.png',
+    },
+    'YRDEN SIGN': {
+        'type': 'SIGN',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615045817/App%20Images/Witcher%203%20Bestiary/Weaknesses/yrden.png',
+    },
+    'AARD': {
+        'type': 'SIGN',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615045786/App%20Images/Witcher%203%20Bestiary/Weaknesses/aard.png',
+    },
+    'AXII': {
+        'type': 'SIGN',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615045903/App%20Images/Witcher%203%20Bestiary/Weaknesses/axii.png',
+    },
+    'DEVIL\'S PUFFBALL': {
+        'type': 'BOMB',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615028176/App%20Images/Witcher%203%20Bestiary/Weaknesses/devils-puffball.png',
+    },
+    'NORTHERN WIND': {
+        'type': 'BOMB',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615028408/App%20Images/Witcher%203%20Bestiary/Weaknesses/northern-wind.png',
+    },
+    'DIMERITIUM BOMB': {
+        'type': 'BOMB',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615028212/App%20Images/Witcher%203%20Bestiary/Weaknesses/dimeritium-bomb.png',
+    },
+    'MOON DUST': {
+        'type': 'BOMB',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615028388/App%20Images/Witcher%203%20Bestiary/Weaknesses/moon-dust.png',
+    },
+    'GRAPESHOT': {
+        'type': 'BOMB',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615028326/App%20Images/Witcher%203%20Bestiary/Weaknesses/grapeshot.png',
+    },
+    'BEAST OIL': {
+        'type': 'OIL',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615027670/App%20Images/Witcher%203%20Bestiary/Weaknesses/beast-oil.png',
+    },
+    'CURSED OIL': {
+        'type': 'OIL',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615028075/App%20Images/Witcher%203%20Bestiary/Weaknesses/cursed-oil.png',
+    },
+    'DRACONID OIL': {
+        'type': 'OIL',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615028046/App%20Images/Witcher%203%20Bestiary/Weaknesses/draconid-oil.png',
+    },
+    'ELEMENTA OIL': {
+        'type': 'OIL',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615053686/App%20Images/Witcher%203%20Bestiary/Weaknesses/elementa-oil.png',
+    },
+    'VAMPIRE OIL': {
+        'type': 'OIL',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615027789/App%20Images/Witcher%203%20Bestiary/Weaknesses/vampire-oil.png',
+    },
+    'BLACK BLOOD': {
+        'type': 'POTION',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615052083/App%20Images/Witcher%203%20Bestiary/Weaknesses/black-blood.png',
+    },
+    'GOLDEN ORIOLE': {
+        'type': 'POTION',
+        'imageUrl': 'https://res.cloudinary.com/dayqxxsip/image/upload/v1615053244/App%20Images/Witcher%203%20Bestiary/Weaknesses/golden-oriole.png',
+    },
+}
