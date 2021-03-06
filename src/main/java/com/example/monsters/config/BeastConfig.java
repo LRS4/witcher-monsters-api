68d5339d9dc47beccda895ca7badf7e0eac67d13
LRS4 <lrspencer@hotmail.co.uk>
2021-03-06 11:39:00 +0000

    Fix paragraph breaks in monster entries

--- src/main/java/com/example/monsters/config/BeastConfig.java
+++ src/main/java/com/example/monsters/config/BeastConfig.java
@@ -44,11 +44,11 @@ public class BeastConfig {
                             "Pussy cat, pussy cat, what did you there?\n" +
                             "Run, fool, that's not a cat but a panther!\n" +
                             "– rare variant of a traditional folk rhyme\n\n" +
-                            "Panthers are dangerous predators found in forests and other woodlands. They are quick, agile and, like all cats, diabolically cunning. In many less-than-thoroughly-urbanized areas, folk still believe panthers are the stranded souls of those who die in their sleep. Superstition thus holds anyone perishing in this way should be dragged to the nearest woods and left there without a burial. The panther-spirit of the deceased may then devour its own body, thereby passing on to the nether realms.\n" +
-                            "This belief must be eradicated by any means, for it leads to epidemics of cholera and other contagious diseases born of rotting corpses.\n" +
-                            "Besides, it is patently ridiculous, given panthers are not necrophages and will not consume carrion of any sort. They prefer more spry and lively prey, such as deer or even humans, provided they are not ill and do not stink too badly.\n" +
-                            "Panthers (and their tawny-coated cousins, mountain lions) are incredibly swift and able to sneak up on prey undetected to gain the advantage of surprise.\n" +
-                            "As soon as they enter open terrain, they leap to attack with astonishing speed, mauling their prey with their sharp claws, long teeth and the sheer force of their momentum.\n" +
+                            "Panthers are dangerous predators found in forests and other woodlands. They are quick, agile and, like all cats, diabolically cunning. In many less-than-thoroughly-urbanized areas, folk still believe panthers are the stranded souls of those who die in their sleep. Superstition thus holds anyone perishing in this way should be dragged to the nearest woods and left there without a burial. The panther-spirit of the deceased may then devour its own body, thereby passing on to the nether realms.\n\n" +
+                            "This belief must be eradicated by any means, for it leads to epidemics of cholera and other contagious diseases born of rotting corpses.\n\n" +
+                            "Besides, it is patently ridiculous, given panthers are not necrophages and will not consume carrion of any sort. They prefer more spry and lively prey, such as deer or even humans, provided they are not ill and do not stink too badly.\n\n" +
+                            "Panthers (and their tawny-coated cousins, mountain lions) are incredibly swift and able to sneak up on prey undetected to gain the advantage of surprise.\n\n" +
+                            "As soon as they enter open terrain, they leap to attack with astonishing speed, mauling their prey with their sharp claws, long teeth and the sheer force of their momentum.\n\n" +
                             "Panthers are known to retreat mid-fight, yet do not let yourself be fooled into a false sense of security. This is but a ruse to allow them to attack again, by surprise. Never drop your guard until you see the animal drop dead.\n\n" +
                             "Panthers are vulnerable to the effects of all the Signs, as well as bombs and oils.",
                     "https://res.cloudinary.com/dayqxxsip/image/upload/v1613822259/App%20Images/Witcher%203%20Bestiary/Beasts/654_tavvhb.png",
@@ -65,12 +65,12 @@ public class BeastConfig {
                     "How doth the little wild boar\n" +
                             "Improve his shining tusks,\n" +
                             "And rut them 'gainst the forest floor,\n" +
-                            "And on the trees' rough husks!\n" +
+                            "And on the trees' rough husks!\n\n" +
                             "How cheerfully he seems to grin\n" +
                             "How neatly spreads his claws,\n" +
                             "And welcomes little lasses in,\n" +
                             "With gently smiling jaws!\n" +
-                            "Louis of Charolle, mathematician, author and poet\n" +
+                            "Louis of Charolle, mathematician, author and poet\n\n" +
                             "These woodland beasts are as \"wild\" – untamed, nasty and aggressive – as their name indicates. To this day, many peasants in outlying Kaedweni villages believe wild boars persist on a diet of young maids, though in truth they most often slate their hunger with roots and acorns. Do not think this vegetarian diet makes them harmless, however. In fact, these animals' stout build and sharp teeth render them veritable fur-covered combat machines. Mother Nature has additionally equipped them with dual pairs of tusks - upper \"pipes\" and lower \"sabres\" - and this weaponry's sum effect is to make the beasts the terrors of the woods, a threat to all who venture or dwell there, human, humanoid and otherwise. Boars also have hard, club-like snouts, which some peasants call their \"whistles,\" though no boar has ever been heard to make such a noise. Instead, they emit a characteristic grunt, which sounds to some like the snoring of an extremely overweight man. Another peasant belief claims boars have extremely prickly natures, and, if offended, will vent their anger by knocking down fences and gobbling up potatoes. Though they sometimes live alone, boars usually appear in small groups of 3 to 5 specimens.",
                     "https://res.cloudinary.com/dayqxxsip/image/upload/v1613822285/App%20Images/Witcher%203%20Bestiary/Beasts/654_xo4mmp.png",
                     "Gustfields,Toussaint",
@@ -84,10 +84,10 @@ public class BeastConfig {
                     "big-bad-wolf",
                     "Big Bad Wolf",
                     "You ask why I have these big hands, my friend? Well, I'll tell you my secret: exercise.\n" +
-                            "— Big Bad Wolf\n" +
-                            "The Big Bad Wolf who lived in the Land of a Thousand Fables was, like the other denizens of that strange sphere, created by Artorius Vigo based on a figure from folk tales. Once he served as a playmate to the duke's daughters, acting out scenes with a certain red-hooded girl and her grandmother, but as the fable land slowly degenerated, so did he.\n" +
-                            "Geralt and Syanna met the Big Bad Wolf while he was nursing a nasty hangover and was not eager to cooperate. Yet since our heroes needed a bean which the wolf had, they forced him to act out his tale.\n" +
-                            "That tale ended as it always had: the Big Bad Wolf attacked the Hunter (played by Geralt), biting and clawing him fiercely while his own wounds healed almost instantaneously. Inevitably, however, the Big Bad Wolf was killed and his stomach cut open.\n" +
+                            "— Big Bad Wolf\n\n" +
+                            "The Big Bad Wolf who lived in the Land of a Thousand Fables was, like the other denizens of that strange sphere, created by Artorius Vigo based on a figure from folk tales. Once he served as a playmate to the duke's daughters, acting out scenes with a certain red-hooded girl and her grandmother, but as the fable land slowly degenerated, so did he.\n\n" +
+                            "Geralt and Syanna met the Big Bad Wolf while he was nursing a nasty hangover and was not eager to cooperate. Yet since our heroes needed a bean which the wolf had, they forced him to act out his tale.\n\n" +
+                            "That tale ended as it always had: the Big Bad Wolf attacked the Hunter (played by Geralt), biting and clawing him fiercely while his own wounds healed almost instantaneously. Inevitably, however, the Big Bad Wolf was killed and his stomach cut open.\n\n" +
                             "This time, out came not a grandma and her granddaughter, but a magic bean.",
                     "https://res.cloudinary.com/dayqxxsip/image/upload/v1613822301/App%20Images/Witcher%203%20Bestiary/Beasts/latest_c75yif.png",
                     "Land of a Thousand Fables",
@@ -100,9 +100,9 @@ public class BeastConfig {
             Monster beastOfBeauclair = new Monster(
                     "the-beast-of-beauclair",
                     "The Beast of Beauclair",
-                    "Eyewitnesses to gruesome monster attacks always have a hard time describing the creature in question. The beasts move quickly and often attack at night, while the witnesses are terrified and primarily concerned with fleeing for their lives. As a result, witchers quite often have no inkling what creature they face until they find tracks or otherwise establish something for themselves. Such was the case with the Beast tormenting Beauclair. Geralt knew it was deadly, elusive and fiendishly clever. Everything else he heard was clearly the product of imaginations fed by fear of a dangerous predator.\n" +
-                            "When Geralt identified the Beast, he discovered it was a higher vampire, meaning one of the most difficult foes a witcher can come to face.\n" +
-                            "Higher vampires are superhumanly strong and fast. They can turn completely invisible, and each comes with its own personalized set of talents and abilities perfected through centuries of existence.\n" +
+                    "Eyewitnesses to gruesome monster attacks always have a hard time describing the creature in question. The beasts move quickly and often attack at night, while the witnesses are terrified and primarily concerned with fleeing for their lives. As a result, witchers quite often have no inkling what creature they face until they find tracks or otherwise establish something for themselves. Such was the case with the Beast tormenting Beauclair. Geralt knew it was deadly, elusive and fiendishly clever. Everything else he heard was clearly the product of imaginations fed by fear of a dangerous predator.\n\n" +
+                            "When Geralt identified the Beast, he discovered it was a higher vampire, meaning one of the most difficult foes a witcher can come to face.\n\n" +
+                            "Higher vampires are superhumanly strong and fast. They can turn completely invisible, and each comes with its own personalized set of talents and abilities perfected through centuries of existence.\n\n" +
                             "When Geralt realized he was dealing with such a creature, he swore foully and realized the rumors about the Beast had not been exaggerated in the slightest.",
                     "https://res.cloudinary.com/dayqxxsip/image/upload/v1613822313/App%20Images/Witcher%203%20Bestiary/Beasts/654_an6fji.png",
                     "Toussaint",
@@ -116,9 +116,9 @@ public class BeastConfig {
                     "wolves",
                     "Wolves",
                     "Friend of mine used to say that with all these griffins and basilisks and whatnot, good old fashioned wolves weren't nothing to worry about... Then the damned beasts devoured half his flock.\n" +
-                            "– Yngvar, shepherd\n" +
-                            "Once upon a time wolves were the absolute rulers of the forest. Men used them to frighten children, while adults, too, trembled at the sound of their howling. Post-Conjunction monsters not only pushed wolves into the deepest wilds but also took over their place in human nightmares. Yet this does not mean the old predators ceased to be a danger. Wolves do not have a drop of magic within them, breathe no fire and spit no acid, but that in no way stops them from killing unwary travelers and hunters.\n" +
-                            "Particularly dangerous are wargs, a nasty and ferocious subspecies of wolf, and the snow-white wolves which today can only be found in the wild highlands of the Skellige archipelago.\n" +
+                            "– Yngvar, shepherd\n\n" +
+                            "Once upon a time wolves were the absolute rulers of the forest. Men used them to frighten children, while adults, too, trembled at the sound of their howling. Post-Conjunction monsters not only pushed wolves into the deepest wilds but also took over their place in human nightmares. Yet this does not mean the old predators ceased to be a danger. Wolves do not have a drop of magic within them, breathe no fire and spit no acid, but that in no way stops them from killing unwary travelers and hunters.\n\n" +
+                            "Particularly dangerous are wargs, a nasty and ferocious subspecies of wolf, and the snow-white wolves which today can only be found in the wild highlands of the Skellige archipelago.\n\n" +
                             "Wolves usually feed in packs counting from a handful to over a dozen members, though some particularly strong males hunt alone. Though wolves are weaker than many post-Conjunction monsters, they make up for any lack of strength with their intelligence. When fighting them one must be especially careful not to become surrounded. The presence of wolves can also signal more serious trouble – these predators often share their hunting grounds with werewolves and leshens.",
                     "https://res.cloudinary.com/dayqxxsip/image/upload/v1613822333/App%20Images/Witcher%203%20Bestiary/Beasts/362_ovi368.png",
                     "Velen,White Orchard,Skellige,Toussaint",
@@ -132,8 +132,8 @@ public class BeastConfig {
                     "dogs",
                     "Dogs",
                     "They say a dog's a man's best friend... But in these foul times one best be wary, even of his friends.\n" +
-                            "– Alfred Pankratz, beggar from Novigrad\n" +
-                            "Some claim dogs are reflections of their masters. A well-treated pet repays his owner with loyalty and trust, whereas an ill-treated one repays this treatment in kind as well, meeting yells with barks and kicks with bites. Since there is an abundance of bad men, so, too, is there an abundance of bad dogs. Most dangerous of all are the vagrant mutts who have lost all respect for men and developed a taste for blood and carrion.\n" +
+                            "– Alfred Pankratz, beggar from Novigrad\n\n" +
+                            "Some claim dogs are reflections of their masters. A well-treated pet repays his owner with loyalty and trust, whereas an ill-treated one repays this treatment in kind as well, meeting yells with barks and kicks with bites. Since there is an abundance of bad men, so, too, is there an abundance of bad dogs. Most dangerous of all are the vagrant mutts who have lost all respect for men and developed a taste for blood and carrion.\n\n" +
                             "Dogs usually attack in packs, choosing isolated victims, especially ones in whom they sense some kind of weakness. Packs of rabid dogs usually only dare attack children, drunkards, the elderly and cripples. Yet when backed into a corner, they will defend themselves with frothing bites and sharp claws against all and sundry.",
                     "https://res.cloudinary.com/dayqxxsip/image/upload/v1613822400/App%20Images/Witcher%203%20Bestiary/Beasts/654_omniqv.png",
                     "Velen,Novigrad,Skellige",
